09bd8ae951bc8a1976b96b62773286968ad4ea69
icywaffle <synkre@gmail.com>
2019-09-18 09:27:50 -0700

    Added Access Control
    Since we're using this as a backend RESTful API, that should only be accessed by my server, we allow full access of control for modifying the database, or grabbing JSON payloads

--- app/init.go
+++ app/init.go
@@ -47,7 +47,9 @@ var HeaderFilter = func(c *revel.Controller, fc []revel.Filter) {
 	c.Response.Out.Header().Add("X-Frame-Options", "SAMEORIGIN")
 	c.Response.Out.Header().Add("X-XSS-Protection", "1; mode=block")
 	c.Response.Out.Header().Add("X-Content-Type-Options", "nosniff")
-	c.Response.Out.Header().Add("Referrer-Policy", "strict-origin-when-cross-origin")
+	//c.Response.Out.Header().Add("Referrer-Policy", "strict-origin-when-cross-origin")
+	c.Response.Out.Header().Add("Access-Control-Allow-Origin", "*")
+	c.Response.Out.Header().Add("Access-Control-Allow-Headers", "*")
 
 	fc[0](c, fc[1:]) // Execute the next filter stage.
 }
