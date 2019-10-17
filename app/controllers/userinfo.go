c906b16ff1ffb71ac8fb218e26d5d9be7f3f0d42
icywaffle <synkre@gmail.com>
2019-10-16 22:23:28 -0700

    Added User Storage Endpoint
    Given a post request, the user can now store any prices related to an item for a specific recipe now

--- app/controllers/userinfo.go
+++ app/controllers/userinfo.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	"fmt"
+	"marketboard-backend/app/controllers/mongoDB"
 	"marketboard-backend/app/models"
 
 	"github.com/revel/revel"
@@ -12,8 +12,12 @@ type UserInfo struct {
 }
 
 func (c UserInfo) Store(userPrice *models.UserPrice) revel.Result {
-	fmt.Println(userPrice)
-
+	UserItemStorage := mongoDB.FindUserItemStorage(UserStorage, userPrice.UserID)
+	if UserItemStorage == nil {
+		mongoDB.InsertNewUserItemStorage(UserStorage, userPrice.UserID)
+	} else {
+		mongoDB.AddUserItem(UserStorage, UserItemStorage, userPrice.UserID, userPrice)
+	}
 	jsonObject := make(map[string]interface{})
 	jsonObject["message"] = "success"
 	return c.RenderJSON(jsonObject)
